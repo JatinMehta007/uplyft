e016caa5f448201dbe1b94c33f8eb59faf5f6c67
Jatin_Mehta <mehtajatin045@gmail.com>
2025-06-09 18:06:32 +0530

    add routes

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { chatQuery } = require('../controllers/chatController');
+const { chatQuery } = require('../controller/chatController');
 
 router.post('/', chatQuery);
 
