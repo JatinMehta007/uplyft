a73eff1ea47ec3945a4b3b484072406edbd699a6
Jatin_Mehta <mehtajatin045@gmail.com>
2025-06-09 18:05:19 +0530

    add react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom"
 import { Signup } from "./components/signup"
 import { Login } from "./components/login"
+import Chat from "./components/chat"
 
 function App() {
  
@@ -12,9 +13,9 @@ function App() {
     <>
     <BrowserRouter>
     <Routes>
-       {/* <Route path="/" element={<Home/>}></Route> */}
         <Route path="/" element={<Signup/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
+        <Route path="/chat" element={<Chat/>}></Route>
         
     </Routes>
     </BrowserRouter>
